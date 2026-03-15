8802d8a487eb8545be24b3e8769746ceb33b4001
velsgearfive <velsgearfive@gmail.com>
2026-03-15 14:02:32 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BusinessWebsite from "./BusinessWebsite";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BusinessWebsite />
+  </React.StrictMode>
+);
